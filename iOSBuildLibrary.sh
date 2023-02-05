18ba217b12262bd071f125cc1a068ce6389505b3
sofia.terekhova <sofiamaria.terekho@aliexpress.ru>
2023-02-05 14:59:31 +0300

    [ZV-186] add logic filter by internal

--- iOSBuildLibrary.sh
+++ iOSBuildLibrary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function getFiles() {
-find . -name "*.proto" ! -name "*internal*.proto" -not -path "*/build/*" -print0 >tmpfile
+find . -name "*.proto" ! -name "*internal*.proto" -not -path "*/build/*" -not -path "*/internal/*" -print0 >tmpfile
 while IFS=  read -r -d $'\0'; do
     protoFiles+=("$REPLY")
 done <tmpfile

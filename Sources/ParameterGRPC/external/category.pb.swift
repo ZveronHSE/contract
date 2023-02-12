d060867716a5348ae1ba5d30de6cb54612959de0
sofia.terekhova <sofiamaria.terekho@aliexpress.ru>
2023-02-12 21:22:45 +0300

    [ZV-323] refactoring to internal and external, delete grpc for others services

--- Sources/ParameterGRPC/external/category.pb.swift
+++ Sources/ParameterGRPC/external/category.pb.swift
@@ -20,18 +20,6 @@ fileprivate struct _GeneratedWithProtocGenSwiftVersion: SwiftProtobuf.ProtobufAP
   typealias Version = _2
 }
 
-public struct CategoryRequest {
-  // SwiftProtobuf.Message conformance is added in an extension below. See the
-  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
-  // methods supported on all messages.
-
-  public var id: Int32 = 0
-
-  public var unknownFields = SwiftProtobuf.UnknownStorage()
-
-  public init() {}
-}
-
 public struct CategoryResponse {
   // SwiftProtobuf.Message conformance is added in an extension below. See the
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
@@ -53,52 +41,21 @@ public struct Category {
 
   public var name: String = String()
 
+  public var imageURL: String = String()
+
   public var unknownFields = SwiftProtobuf.UnknownStorage()
 
   public init() {}
 }
 
 #if swift(>=5.5) && canImport(_Concurrency)
-extension CategoryRequest: @unchecked Sendable {}
 extension CategoryResponse: @unchecked Sendable {}
 extension Category: @unchecked Sendable {}
 #endif  // swift(>=5.5) && canImport(_Concurrency)
 
 // MARK: - Code below here is support for the SwiftProtobuf runtime.
 
-fileprivate let _protobuf_package = "ru.zveron.contract.category"
-
-extension CategoryRequest: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  public static let protoMessageName: String = _protobuf_package + ".CategoryRequest"
-  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
-    1: .same(proto: "id"),
-  ]
-
-  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
-    while let fieldNumber = try decoder.nextFieldNumber() {
-      // The use of inline closures is to circumvent an issue where the compiler
-      // allocates stack space for every case branch when no optimizations are
-      // enabled. https://github.com/apple/swift-protobuf/issues/1034
-      switch fieldNumber {
-      case 1: try { try decoder.decodeSingularInt32Field(value: &self.id) }()
-      default: break
-      }
-    }
-  }
-
-  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
-    if self.id != 0 {
-      try visitor.visitSingularInt32Field(value: self.id, fieldNumber: 1)
-    }
-    try unknownFields.traverse(visitor: &visitor)
-  }
-
-  public static func ==(lhs: CategoryRequest, rhs: CategoryRequest) -> Bool {
-    if lhs.id != rhs.id {return false}
-    if lhs.unknownFields != rhs.unknownFields {return false}
-    return true
-  }
-}
+fileprivate let _protobuf_package = "ru.zveron.contract.parameter.external"
 
 extension CategoryResponse: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
   public static let protoMessageName: String = _protobuf_package + ".CategoryResponse"
@@ -137,6 +94,7 @@ extension Category: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationB
   public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
     1: .same(proto: "id"),
     2: .same(proto: "name"),
+    3: .standard(proto: "image_url"),
   ]
 
   public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
@@ -147,6 +105,7 @@ extension Category: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationB
       switch fieldNumber {
       case 1: try { try decoder.decodeSingularInt32Field(value: &self.id) }()
       case 2: try { try decoder.decodeSingularStringField(value: &self.name) }()
+      case 3: try { try decoder.decodeSingularStringField(value: &self.imageURL) }()
       default: break
       }
     }
@@ -159,12 +118,16 @@ extension Category: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationB
     if !self.name.isEmpty {
       try visitor.visitSingularStringField(value: self.name, fieldNumber: 2)
     }
+    if !self.imageURL.isEmpty {
+      try visitor.visitSingularStringField(value: self.imageURL, fieldNumber: 3)
+    }
     try unknownFields.traverse(visitor: &visitor)
   }
 
   public static func ==(lhs: Category, rhs: Category) -> Bool {
     if lhs.id != rhs.id {return false}
     if lhs.name != rhs.name {return false}
+    if lhs.imageURL != rhs.imageURL {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }
--- Sources/ParameterGRPC/external/parameter.pb.swift
+++ Sources/ParameterGRPC/external/parameter.pb.swift
@@ -116,19 +116,26 @@ public struct Parameter {
   public init() {}
 }
 
-public struct ParameterValueRequest {
+public struct LotFormResponse {
   // SwiftProtobuf.Message conformance is added in an extension below. See the
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  /// Идентификатор категория.
-  public var categoryID: Int32 = 0
+  public var lotForms: [LotForm] = []
 
-  /// Идентификатор типа объявления.
-  public var lotFormID: Int32 = 0
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
 
-  /// Значения для каждого параметра по id/string
-  public var parameterValues: Dictionary<Int32,String> = [:]
+  public init() {}
+}
+
+public struct LotForm {
+  // SwiftProtobuf.Message conformance is added in an extension below. See the
+  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+  // methods supported on all messages.
+
+  public var id: Int32 = 0
+
+  public var name: String = String()
 
   public var unknownFields = SwiftProtobuf.UnknownStorage()
 
@@ -140,12 +147,13 @@ extension TypeEnum: @unchecked Sendable {}
 extension ParameterRequest: @unchecked Sendable {}
 extension ParameterResponse: @unchecked Sendable {}
 extension Parameter: @unchecked Sendable {}
-extension ParameterValueRequest: @unchecked Sendable {}
+extension LotFormResponse: @unchecked Sendable {}
+extension LotForm: @unchecked Sendable {}
 #endif  // swift(>=5.5) && canImport(_Concurrency)
 
 // MARK: - Code below here is support for the SwiftProtobuf runtime.
 
-fileprivate let _protobuf_package = "ru.zveron.contract.parameter"
+fileprivate let _protobuf_package = "ru.zveron.contract.parameter.external"
 
 extension TypeEnum: SwiftProtobuf._ProtoNameProviding {
   public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
@@ -281,12 +289,10 @@ extension Parameter: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementation
   }
 }
 
-extension ParameterValueRequest: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  public static let protoMessageName: String = _protobuf_package + ".ParameterValueRequest"
+extension LotFormResponse: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = _protobuf_package + ".LotFormResponse"
   public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
-    1: .standard(proto: "category_id"),
-    2: .standard(proto: "lot_form_id"),
-    3: .standard(proto: "parameter_values"),
+    1: .standard(proto: "lot_forms"),
   ]
 
   public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
@@ -295,31 +301,59 @@ extension ParameterValueRequest: SwiftProtobuf.Message, SwiftProtobuf._MessageIm
       // allocates stack space for every case branch when no optimizations are
       // enabled. https://github.com/apple/swift-protobuf/issues/1034
       switch fieldNumber {
-      case 1: try { try decoder.decodeSingularInt32Field(value: &self.categoryID) }()
-      case 2: try { try decoder.decodeSingularInt32Field(value: &self.lotFormID) }()
-      case 3: try { try decoder.decodeMapField(fieldType: SwiftProtobuf._ProtobufMap<SwiftProtobuf.ProtobufInt32,SwiftProtobuf.ProtobufString>.self, value: &self.parameterValues) }()
+      case 1: try { try decoder.decodeRepeatedMessageField(value: &self.lotForms) }()
       default: break
       }
     }
   }
 
   public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
-    if self.categoryID != 0 {
-      try visitor.visitSingularInt32Field(value: self.categoryID, fieldNumber: 1)
-    }
-    if self.lotFormID != 0 {
-      try visitor.visitSingularInt32Field(value: self.lotFormID, fieldNumber: 2)
-    }
-    if !self.parameterValues.isEmpty {
-      try visitor.visitMapField(fieldType: SwiftProtobuf._ProtobufMap<SwiftProtobuf.ProtobufInt32,SwiftProtobuf.ProtobufString>.self, value: self.parameterValues, fieldNumber: 3)
+    if !self.lotForms.isEmpty {
+      try visitor.visitRepeatedMessageField(value: self.lotForms, fieldNumber: 1)
     }
     try unknownFields.traverse(visitor: &visitor)
   }
 
-  public static func ==(lhs: ParameterValueRequest, rhs: ParameterValueRequest) -> Bool {
-    if lhs.categoryID != rhs.categoryID {return false}
-    if lhs.lotFormID != rhs.lotFormID {return false}
-    if lhs.parameterValues != rhs.parameterValues {return false}
+  public static func ==(lhs: LotFormResponse, rhs: LotFormResponse) -> Bool {
+    if lhs.lotForms != rhs.lotForms {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension LotForm: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = _protobuf_package + ".LotForm"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .same(proto: "id"),
+    2: .same(proto: "name"),
+  ]
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeSingularInt32Field(value: &self.id) }()
+      case 2: try { try decoder.decodeSingularStringField(value: &self.name) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    if self.id != 0 {
+      try visitor.visitSingularInt32Field(value: self.id, fieldNumber: 1)
+    }
+    if !self.name.isEmpty {
+      try visitor.visitSingularStringField(value: self.name, fieldNumber: 2)
+    }
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: LotForm, rhs: LotForm) -> Bool {
+    if lhs.id != rhs.id {return false}
+    if lhs.name != rhs.name {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }

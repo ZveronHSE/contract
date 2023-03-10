4771d0d65f9bb4a508656316832a979e70c63840
sofia.terekhova <sofiamaria.terekho@aliexpress.ru>
2023-03-10 15:52:28 +0300

    [ZV-347] redone gradle and proto contract, extract to core module

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let productName = "ZveronGRPC"
 let apigatewayTargetName = "ApigatewayGRPC"
 let authTargetName = "AuthGRPC"
 let blacklistTargetName = "BlacklistGRPC"
+let coreTargetName = "CoreGRPC"
 let favoritesTargetName = "FavoritesGRPC"
 let lotTargetName = "LotGRPC"
 let parameterTargetName = "ParameterGRPC"
@@ -27,6 +28,7 @@ let package = Package(
                 apigatewayTargetName,
                 authTargetName,
                 blacklistTargetName,
+                coreTargetName,
                 favoritesTargetName,
                 lotTargetName,
                 parameterTargetName,
@@ -39,6 +41,7 @@ let package = Package(
         .target(name: apigatewayTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: authTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: blacklistTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
+        .target(name: coreTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: favoritesTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: lotTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: parameterTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),

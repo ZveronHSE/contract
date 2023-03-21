ab5398c0cec88dbdc3ad1dcc54b14384d2c56e4e
KIRILL PIQUE <kirill.pique@alibaba-inc.com>
2023-03-21 11:07:39 +0300

    ZV-322: return image url instead of id after upload

--- Sources/ObjectstorageGRPC/external/bucket.pb.swift
+++ Sources/ObjectstorageGRPC/external/bucket.pb.swift
@@ -130,7 +130,7 @@ public struct UploadImageResponse {
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  public var imageID: Int64 = 0
+  public var imageURL: String = String()
 
   public var unknownFields = SwiftProtobuf.UnknownStorage()
 
@@ -211,7 +211,7 @@ extension UploadImageRequest: SwiftProtobuf.Message, SwiftProtobuf._MessageImple
 extension UploadImageResponse: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
   public static let protoMessageName: String = _protobuf_package + ".UploadImageResponse"
   public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
-    1: .standard(proto: "image_id"),
+    1: .standard(proto: "image_url"),
   ]
 
   public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
@@ -220,21 +220,21 @@ extension UploadImageResponse: SwiftProtobuf.Message, SwiftProtobuf._MessageImpl
       // allocates stack space for every case branch when no optimizations are
       // enabled. https://github.com/apple/swift-protobuf/issues/1034
       switch fieldNumber {
-      case 1: try { try decoder.decodeSingularInt64Field(value: &self.imageID) }()
+      case 1: try { try decoder.decodeSingularStringField(value: &self.imageURL) }()
       default: break
       }
     }
   }
 
   public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
-    if self.imageID != 0 {
-      try visitor.visitSingularInt64Field(value: self.imageID, fieldNumber: 1)
+    if !self.imageURL.isEmpty {
+      try visitor.visitSingularStringField(value: self.imageURL, fieldNumber: 1)
     }
     try unknownFields.traverse(visitor: &visitor)
   }
 
   public static func ==(lhs: UploadImageResponse, rhs: UploadImageResponse) -> Bool {
-    if lhs.imageID != rhs.imageID {return false}
+    if lhs.imageURL != rhs.imageURL {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }

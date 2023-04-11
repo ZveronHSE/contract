4be201ffb81a3154dca53eb98ab61a1b40ba97f4
Dmitry Kochik <55440084+dakochik@users.noreply.github.com>
2023-04-11 20:28:44 +0300

    ZV-000: Missed swift code (#46)
    Co-authored-by: Dmitry Kochik <dakochik@yandex-team.ru>

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let productName = "ZveronGRPC"
 let apigatewayTargetName = "ApigatewayGRPC"
 let authTargetName = "AuthGRPC"
 let blacklistTargetName = "BlacklistGRPC"
+let chatTargetName = "ChatGRPC"
 let coreTargetName = "CoreGRPC"
 let favoritesTargetName = "FavoritesGRPC"
 let lotTargetName = "LotGRPC"
@@ -29,6 +30,7 @@ let package = Package(
                 apigatewayTargetName,
                 authTargetName,
                 blacklistTargetName,
+                chatTargetName,
                 coreTargetName,
                 favoritesTargetName,
                 lotTargetName,
@@ -43,6 +45,7 @@ let package = Package(
         .target(name: apigatewayTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: authTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: blacklistTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
+        .target(name: chatTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: coreTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: favoritesTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
         .target(name: lotTargetName, dependencies: [.product(name:"GRPC", package: "grpc-swift")]),
